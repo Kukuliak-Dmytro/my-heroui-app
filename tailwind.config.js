179b838a2d2e0e759667330a9902fa09292894c1
Kukuliak-Dmytro <dmytro.kukuliak.oi.2025@lpnu.ua>
2025-10-14 12:35:07 +0300

    Tailwind theme variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 import { heroui } from "@heroui/theme"
 
+// if I remember correctly, tailwind config is legacy starting with TW v4
+// the new approach is to define the variables in the theme object, CSS file
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
@@ -20,38 +23,107 @@ const config = {
     themes: {
       light: {
         colors: {
-          background: "#FFFFFF", // or DEFAULT
-          foreground: "#11181C", // or 50 to 900 DEFAULT
+          background: "#fafaf9", // stone-50
+          foreground: "#292524", // stone-800
           primary: {
-            //... 50 to 900
-            foreground: "#FFFFFF",
-            DEFAULT: "#006FEE",
+            50: "#fffbeb",
+            100: "#fef3c7",
+            200: "#fde68a",
+            300: "#fcd34d",
+            400: "#fbbf24",
+            500: "#f59e0b",
+            600: "#d97706",
+            700: "#b45309",
+            800: "#92400e",
+            900: "#78350f",
+            950: "#451a03",
+            foreground: "#451a03",
+            DEFAULT: "#f59e0b",
           },
-          // ... rest of the colors
+          secondary: {
+            50: "#ecfdf5",
+            100: "#d1fae5",
+            200: "#a7f3d0",
+            300: "#6ee7b7",
+            400: "#34d399",
+            500: "#10b981",
+            600: "#059669",
+            700: "#047857",
+            800: "#065f46",
+            900: "#064e3b",
+            950: "#022c22",
+            foreground: "#ffffff",
+            DEFAULT: "#047857",
+          },
+          danger: {
+            50: "#fff1f2",
+            100: "#ffe4e6",
+            200: "#fecdd3",
+            300: "#fda4af",
+            400: "#fb7185",
+            500: "#f43f5e",
+            600: "#e11d48",
+            700: "#be123c",
+            800: "#9f1239",
+            900: "#881337",
+            950: "#4c0519",
+            foreground: "#ffffff",
+            DEFAULT: "#9f1239",
+          },
+
         },
       },
       dark: {
         colors: {
-          background: "#000000", // or DEFAULT
-          foreground: "#ECEDEE", // or 50 to 900 DEFAULT
+          background: "#1c1917", // stone-900
+          foreground: "#d6d3d1", // stone-300
           primary: {
-            //... 50 to 900
-            foreground: "#FFFFFF",
-            DEFAULT: "#006FEE",
+            50: "#fffbeb",
+            100: "#fef3c7",
+            200: "#fde68a",
+            300: "#fcd34d",
+            400: "#fbbf24",
+            500: "#f59e0b",
+            600: "#d97706",
+            700: "#b45309",
+            800: "#92400e",
+            900: "#78350f",
+            950: "#451a03",
+            foreground: "#451a03",
+            DEFAULT: "#f59e0b",
+          },
+          secondary: {
+            50: "#ecfdf5",
+            100: "#d1fae5",
+            200: "#a7f3d0",
+            300: "#6ee7b7",
+            400: "#34d399",
+            500: "#10b981",
+            600: "#059669",
+            700: "#047857",
+            800: "#065f46",
+            900: "#064e3b",
+            950: "#022c22",
+            foreground: "#ffffff",
+            DEFAULT: "#059669",
+          },
+          danger: {
+            50: "#fff1f2",
+            100: "#ffe4e6",
+            200: "#fecdd3",
+            300: "#fda4af",
+            400: "#fb7185",
+            500: "#f43f5e",
+            600: "#e11d48",
+            700: "#be123c",
+            800: "#9f1239",
+            900: "#881337",
+            950: "#4c0519",
+            foreground: "#ffffff",
+            DEFAULT: "#be123c",
           },
         },
         // ... rest of the colors
-      },
-      mytheme: {
-        // custom theme
-        extend: "dark",
-        colors: {
-          primary: {
-            DEFAULT: "#BEF264",
-            foreground: "#000000",
-          },
-          focus: "#BEF264",
-        },
       },
     },
   }),],
