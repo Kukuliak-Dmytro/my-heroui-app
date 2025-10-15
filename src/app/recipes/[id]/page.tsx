4d3422ee64bbb723f13e30b9f67fe36f04ef58a9
Kukuliak-Dmytro <dmytro.kukuliak.oi.2025@lpnu.ua>
2025-10-15 18:11:14 +0300

    feat: implement 30-second revalidation for recipe pages and adjust query options for caching
    - src/app/recipes/page.tsx: add revalidation setting for recipe listing page
    - src/app/recipes/[id]/page.tsx: implement revalidation for single recipe page
    - src/entities/api/recipes/recipe.query.ts: update query options to align stale time and refetch intervals with revalidation period

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { SingleRecipePageComponent } from "@/modules/recipes/single-recipe-page/single-page-recipe.component";
 
+export const revalidate = 30;
+
 export default async function Page({
   params,
 }: {
--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -4,6 +4,9 @@ interface IRecipesPageProps {
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
+
+export const revalidate = 30;
+
 export default async function RecipesPage({ searchParams }: IRecipesPageProps) {
     const resolvedSearchParams = await searchParams;
     return (
